a6e956084f075cf86bfcfdd0528c1118aacd67ea
Karen Oliver <oliverkl@unimelb.edu.au>
2023-12-06 14:05:40 +1100

    Update LMM code.R

--- Primary analyses/LMM code.R
+++ Primary analyses/LMM code.R
@@ -1,14 +1,24 @@
 library(lme4)
 library(lmerTest)
 
-primary <- glmer(outcome2 ~ `allEpi3_0.5.norm` + SEX + cohort + FS + RV 
+# PRS models tested:
+## `allEpi_0.5.PRS.norm`
+## `ggeEpi_0.5.PRS.norm`
+## `focalEpi_0.5.PRS.norm`
+## `fs_0.5.PRS.norm`
+
+primary <- glmer(outcome2 ~ `allEpi_0.5.PRS.norm` + SEX + cohort + FS + RV 
                                 + PC1.std + PC2.std + PC3.std + PC4.std + PC5.std +
-                     (1|PFN2), family = binomial, data = gefsplus_database_nocntrl)
+                     (1|PFN2), family = binomial, data = gefsplus_database)
 
 summary(primary)
 
-#calc OR
+# calc OR
 cc <- confint(primary,parm="beta_", method="Wald")
 ctab <- cbind(est=fixef(primary),cc)
 rtab <- exp(ctab)
 print(rtab,digits=3)
+
+# correct for multiple testing
+p_vals <- c(0.01,0.04,0.11,0.31)
+p.adjust (p_vals, method="bonferroni")
